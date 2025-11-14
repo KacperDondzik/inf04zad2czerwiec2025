43edc28324965e7184cd7ae036878b6842ed3c6a
kDondzik <dondzik.kacper@szkola.elektrykplock.edu.pl>
2025-11-14 11:22:23 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace inf04zad2czerwiec2025
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
